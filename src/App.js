2a4d3086777c01aa9dd4c4e124ced561e144e803
Jay <mfpjayb@gmail.com>
2020-09-05 21:29:35 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+<template>
+    <h1>Maf.js Starter App</h1>
+</template>
+
+export default class App { }
+
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { Module } from 'maf.js';
+import App from './App';
+
+const module = new Module({
+    component: App
+});
+
+module.$mount('#app', true);
